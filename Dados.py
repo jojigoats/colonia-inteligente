ae075d9c5bcfe88abe965903a7b1de097df14f8b
Julio Joaquim <125167699+jojigoats@users.noreply.github.com>
2026-05-23 13:29:57 -0300

    Initialize colony data structure in Dados.py
    Added data structure for colony energy, consumption, and climate.

---
+++ Dados.py
@@ -0,0 +1,23 @@
+# =========================================
+# DADOS DA COLÔNIA
+# =========================================
+
+colonia = {
+    "energia": {
+        "solar": 40,
+        "eolica": 30,
+        "reserva": 20
+    },
+
+    "consumo": {
+        "suporte_vida": 35,
+        "laboratorio": 20,
+        "comunicacao": 10,
+        "lazer": 15
+    },
+
+    "clima": {
+        "vento": 11,
+        "temperatura": -20
+    }
+}
